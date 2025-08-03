0d5035bd0152375e0d4cf082f22ae39097cd0f58
ravichandran2006 <ravichandranm.23aim@kongu.edu>
2025-08-03 23:21:59 +0530

    backend normal changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const tenderRoutes = require('./routes/tenderRoutes');
+const tenderRoutes = require('./routes/tenderRoutes ');
 const path = require('path');
 
 dotenv.config();
